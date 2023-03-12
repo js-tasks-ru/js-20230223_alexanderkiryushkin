74944d5a573c02c358c66b6c4fd83bab565c3b34
Alexander <alexanderkiryushkin@yandex.ru>
2023-03-12 23:17:08 +0300

    5 lesson notify

--- 05-dom-document-loading/1-notification/index.js
+++ 05-dom-document-loading/1-notification/index.js
@@ -1,3 +1,69 @@
 export default class NotificationMessage {
 
+
+  constructor(message, {
+    duration,
+    type
+  } = {}) {
+    this.message = message;
+    this.duration = duration;
+    this.type = type;
+
+    this.template = `
+    <div class="notification ${this.type}" style="--value:${this.duration / 1000}s">
+    <div class="timer"></div>
+    <div class="inner-wrapper">
+      ${this.getNotifyHeader()}
+      ${this.getNotifyBody()}
+      </div>
+    </div>
+    `;
+
+    this.render();
+  }
+
+
+  getNotifyHeader() {
+    return `
+  <div class="notification-header">
+  ${this.type}
+  </div>
+  `;
+  }
+
+
+  getNotifyBody() {
+    return `
+    <div class="notification-body">
+    ${this.message}
+  </div>
+    `;
+  }
+
+
+  render() {
+
+
+    const tempWrapper = document.createElement('div');
+    tempWrapper.innerHTML = this.template;
+    this.element = tempWrapper.firstElementChild;
+  }
+
+
+  show(target = document.body) {
+
+    target.append(this.element);
+    setTimeout(() => this.remove(), this.duration);
+
+  }
+
+
+  remove() {
+    this.element.remove();
+  }
+
+
+  destroy() {
+    this.remove();
+  }
 }
